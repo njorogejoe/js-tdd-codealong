fdcbe8a6239d89d14b4e57dcb204d282d5c491dc
Joseph <mwanikij118@gmail.com>
2025-07-25 17:26:59 +0300

    Adds utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
 // Your code here
+// utils.js
+
+/**
+ * Calculates the current age based on the provided birth year.
+ * @param {number} birthYear - The year the person was born.
+ * @returns {number} The person's current age.
+ */
+export function currentAgeForBirthYear(birthYear) {
+  return new Date().getFullYear() - birthYear;
+}
